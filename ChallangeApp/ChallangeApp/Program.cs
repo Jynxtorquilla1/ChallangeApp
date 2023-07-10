253d97a975bd5226b6fc5472753aa4d0659eeaba
Jynxtorquilla1 <wojciech.szk@gmail.com>
2023-07-10 20:05:23 +0200

    Zadanie domowe dzień 4

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+bool female;
+female = true;
+var age = 33;
+
+if (female == true)
+{
+    if (name == "Ewa" && age == 33)
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+    else if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else 
+    {
+        Console.WriteLine("Kobieta w wieku 30 lat lub powyżej");
+    }
+}
+else
+{
+    if (age < 18)
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna");
+    }
+    else
+    {
+        Console.WriteLine("Pełnoletni mężczyzna");
+    }
+}
