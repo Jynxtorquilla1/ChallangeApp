9a137a4dff24d475ebc0074699ee5f735a95a187
Jynxtorquilla1 <wojciech.szk@gmail.com>
2023-07-15 21:10:35 +0200

    Zadanie domowe dzień 6

---
+++ ChallangeApp/ChallangeApp/Employee.cs
@@ -0,0 +1,32 @@
+﻿namespace ChallangeApp
+{
+    class Employee
+    {
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public int Age { get; private set; }
+
+        private List<int> mark = new List<int>();
+
+        public Employee(string name, string surname, int age)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+        }
+
+        public void addMark(int mark)
+        {
+            this.mark.Add(mark);
+        }
+
+        public int Result
+        {
+            get
+            {
+                return mark.Sum();
+            }
+
+        }
+    }
+}
--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,17 +1,26 @@
-﻿
-using ChallangeApp;
-using System;
-using System.Runtime.CompilerServices;
+﻿using ChallangeApp;
 
 Employee empl1 = new Employee("Jack  ", "Black", 53);
 Employee empl2 = new Employee("Elon  ", "Musk ", 52);
 Employee empl3 = new Employee("Donald", "Trump", 77);
 
-empl1.addScore(9); empl1.addScore(9); empl1.addScore(7); empl1.addScore(7); empl1.addScore(10);
+empl1.addMark(9);
+empl1.addMark(9);
+empl1.addMark(7);
+empl1.addMark(7);
+empl1.addMark(10);
 
-empl2.addScore(2); empl2.addScore(2); empl2.addScore(5); empl2.addScore(3); empl2.addScore(3);
+empl2.addMark(2);
+empl2.addMark(2);
+empl2.addMark(5);
+empl2.addMark(3);
+empl2.addMark(3);
 
-empl3.addScore(1); empl3.addScore(2); empl3.addScore(1); empl3.addScore(1); empl3.addScore(1);
+empl3.addMark(1);
+empl3.addMark(2);
+empl3.addMark(1);
+empl3.addMark(1);
+empl3.addMark(1);
 
 //Console.WriteLine(empl2.Result);
 
@@ -21,22 +30,24 @@ List<Employee> employees = new List<Employee>()
     empl1, empl2, empl3
 };
 
-int empl1Score = empl1.Result;
-int empl2Score = empl2.Result;
-int empl3Score = empl3.Result;
-int maxScore = Math.Max(empl1Score, Math.Max(empl2Score, empl3Score));
+int empl1Mark = empl1.Result;
+int empl2Mark = empl2.Result;
+int empl3Mark = empl3.Result;
+int maxMark = Math.Max(empl1Mark, Math.Max(empl2Mark, empl3Mark));
+
 //Console.WriteLine(maxScore);
+
 Console.WriteLine("Name" + "   Surname" + "  Age  " + "Score");
 Console.WriteLine("       ");
 
 foreach (var empl in employees)
 {
-    if (empl.Result == maxScore)
+    if (empl.Result == maxMark)
     {
-        Console.WriteLine(empl.Name + "   " + empl.Surname +"   "+ empl.Age + "   " + empl.Result);
+        Console.WriteLine(empl.Name + "   " + empl.Surname +"   "+ empl.Age + "   " + empl.Result + "   Employee with the best result");
     }
     else
     {
-        Console.WriteLine(empl.Name + "   " + empl.Surname + "   " + empl.Age + "   Loser");
+        Console.WriteLine(empl.Name + "   " + empl.Surname + "   " + empl.Age +"   " + empl.Result + "   Loser");
     }
 }
