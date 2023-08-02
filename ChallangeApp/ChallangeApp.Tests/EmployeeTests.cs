ceba9dc035c9a367e525e2d05915ff8ff7eb9a90
Jynxtorquilla1 <wojciech.szk@gmail.com>
2023-08-02 10:23:40 +0200

    Zadanie domowe dzień 11

--- ChallangeApp/ChallangeApp.Tests/EmployeeTests.cs
+++ ChallangeApp/ChallangeApp.Tests/EmployeeTests.cs
@@ -2,21 +2,22 @@ namespace ChallangeApp.Tests
 {
     public class EmployeeTests
     {
+
         [Test]
         public void WhenMarksAreGiven_ShouldReturnCorrectMaxMinAndAvarageValues()
         {
             //arrange
 
             var employee1 = new Employee("Ferdynand", "Kiepski");
-            employee1.addMark(7);
-            employee1.addMark(9);
-            employee1.addMark(2);
-            employee1.addMark(5);
+            employee1.AddMark(7);
+            employee1.AddMark(9);
+            employee1.AddMark(2);
+            employee1.AddMark(5);
             float avarage = 5.75f;
-            
+
             //act
 
-            var statistics = employee1.GetStatistics();
+            var statistics = employee1.GetStatisticsWithForeach();
 
             //assert
 
@@ -26,6 +27,57 @@ namespace ChallangeApp.Tests
 
         }
 
+        [Test]
+
+        public void WhenMarksAreGiven_ThreeDifferentMethodsShouldReturnSameValues()
+        {
+            //arrange
+
+            var employee1 = new Employee("Ferdynand", "Kiepski");
+            employee1.AddMark(7);
+            employee1.AddMark(9);
+            employee1.AddMark(2);
+            employee1.AddMark(5);
+            float avarage = 5.75f;
+
+            //act
+
+            var statisticsForeach = employee1.GetStatisticsWithForeach();
+            var statisticsFor = employee1.GetStatisticsWithFor();
+            var statisticsWhile = employee1.GetStatisticsWithWhile();
+            var statisticsDoWhile = employee1.GetStatisticsWithDoWhile();
+
+            List<float> statsForeachTest = new List<float>
+            {
+                statisticsForeach.Avarage, statisticsForeach.Min, statisticsForeach.Max
+            };
+            
+            List<float> statsForTest = new List<float> 
+            {
+                statisticsFor.Avarage, statisticsFor.Min, statisticsFor.Max
+            };
+            
+            List<float> statsWhile = new List<float> 
+            {
+                statisticsWhile.Avarage, statisticsWhile.Min, statisticsWhile.Max 
+            };
+           
+            List<float> statsDoWhile = new List<float>
+            { 
+                statisticsDoWhile.Avarage, statisticsDoWhile.Min, statisticsDoWhile.Max
+            };
+
+            bool ForForeachEquality = statsForeachTest.SequenceEqual(statsForTest);
+            bool WhileDoWhileEquality = statsWhile.SequenceEqual(statsDoWhile);
+            bool ForWhileEquality = statsWhile.SequenceEqual(statsDoWhile);
+
+            //assert
+
+            Assert.AreEqual(true, ForForeachEquality);
+            Assert.AreEqual(true, WhileDoWhileEquality);
+            Assert.AreEqual(true, ForWhileEquality);
+
+        }
 
     }
 }
--- ChallangeApp/ChallangeApp/Employee.cs
+++ ChallangeApp/ChallangeApp/Employee.cs
@@ -1,4 +1,7 @@
-﻿namespace ChallangeApp
+﻿using System.Runtime.Intrinsics.Arm;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ChallangeApp
 {
     public class Employee
     {
@@ -8,24 +11,24 @@
         private List<float> marks = new List<float>();
 
         public Employee(string name, string surname)
-        { 
+        {
             this.Name = name;
             this.Surname = surname;
         }
-        
+
         public void AddMark(float mark)
         {
             if (mark >= 0 && mark < 100)
             {
                 this.marks.Add(mark);
             }
-            else 
+            else
             {
                 Console.WriteLine($"{mark} is invalid mark value");
             }
-            
+
         }
-        
+
         public void AddMark(string mark)
         {
             if (float.TryParse(mark, out float result))
@@ -37,7 +40,7 @@
                 Console.WriteLine($"string {mark} is not float ");
             }
         }
-                       
+
         public void AddMark(double mark)
         {
             float doubleInFloat = (float)Math.Round(mark, 2);
@@ -56,7 +59,7 @@
             this.AddMark(doubleInDecimal);
         }
 
-        public Statistics GetStatistics()
+        public Statistics GetStatisticsWithForeach()
         {
             var statistics = new Statistics();
 
@@ -73,9 +76,64 @@
 
             statistics.Avarage /= this.marks.Count;
 
-
             return statistics;
         }
 
+        public Statistics GetStatisticsWithFor()
+        {
+            var statistics = new Statistics();
+            statistics.Avarage = 0;
+            statistics.Max = float.MinValue;
+            statistics.Min = float.MaxValue;
+
+            for (var index = 0; index < marks.Count; index++)
+            {
+                statistics.Max = Math.Max(statistics.Max, marks[index]);
+                statistics.Min = Math.Min(statistics.Min, marks[index]);
+                statistics.Avarage += marks[index];
+            }
+            statistics.Avarage /= marks.Count;
+            return statistics;
+        }
+
+
+        public Statistics GetStatisticsWithWhile()
+        {
+            var statistics = new Statistics();
+            statistics.Avarage = 0;
+            statistics.Max = float.MinValue;
+            statistics.Min = float.MaxValue;
+            var index = 0;
+
+            while (index < this.marks.Count)
+            {
+                statistics.Max = Math.Max(statistics.Max, this.marks[index]);
+                statistics.Min = Math.Min(statistics.Min, this.marks[index]);
+                statistics.Avarage += this.marks[index];
+                index++;
+            }
+            statistics.Avarage /= marks.Count;
+            return statistics;
+        }
+
+        public Statistics GetStatisticsWithDoWhile()
+        {
+            var statistics = new Statistics();
+            statistics.Avarage = 0;
+            statistics.Max = float.MinValue;
+            statistics.Min = float.MaxValue;
+            var index = 0;
+
+            do
+            {
+                statistics.Max = Math.Max(statistics.Max, this.marks[index]);
+                statistics.Min = Math.Min(statistics.Min, this.marks[index]);
+                statistics.Avarage += this.marks[index];
+                index++;
+            } while (index < this.marks.Count);
+
+            statistics.Avarage /= marks.Count;
+            return statistics;
+        }
     }
 }
